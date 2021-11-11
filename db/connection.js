2c058a0fdebcc26fbe42daa207fbe8e367aaf8c6
EmilyT <emilython@hotmail.com>
2021-11-11 15:24:14 -0600

    added connection.js

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+// dependencies
+// ======================================
+const mysql = require('mysql2');
+
+// Connect to database
+// ======================================
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'Mootoo2!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  // Module exports
+// =========================================
+  module.exports = db;
