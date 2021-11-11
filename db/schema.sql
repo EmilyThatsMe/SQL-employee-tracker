01a1ce37500fea9be708f903bd50e13730d888f8
EmilyT <emilython@hotmail.com>
2021-11-11 16:28:26 -0600

    created departments table

---
+++ db/schema.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS departments;
+
+CREATE TABLE departments (
+     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+     department_name VARCHAR(30) NOT NULL,
+     department_id VARCHAR(30) NOT NULL
+     );
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO departments (department_name, department_id)
+VALUES('Ministry of Magic', 1),
+('Candy Deparment', 2),
+('Criminal Justice', 3),
+('Party Business Only', 4),
+('Code Monkeys', 5)
